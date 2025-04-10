82957210b798ca34cef177dc5d1c5f786c43dcdb
smash-datasci <vivertido@proton.me>
2025-04-10 20:07:06 +0000

    first commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,38 @@
 import streamlit as st
+import  requests  
 
-st.title("🎈 My new app")
+def get_api_data(test_url):
+
+    try:
+        response = requests.get(test_url)
+   
+        data = response.json()
+        
+
+        return data
+   
+
+    except Exception as e:
+        return(f"❌ Error: {e}")
+
+
+st.title("API explorer")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Paste your base URL below:"
 )
+
+input_url = st.text_input("URL", "")
+
+st.write("The URL  is", input_url)
+
+if st.button("Get API Data"):
+
+    if input_url != "":
+
+        data = get_api_data(input_url)
+        st.write(data)
+
+        
+     
+
+
